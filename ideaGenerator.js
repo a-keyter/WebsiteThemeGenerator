d3b0f5851ae4fa910c9aceed6cfdfa7fd5e34095
a-keyter <alexkeyter@gmail.com>
2022-06-27 11:37:52 +0100

    Completed the Design Brief Generator

--- ideaGenerator.js
+++ ideaGenerator.js
@@ -2,10 +2,10 @@
 
 
 
-var pageTypes = ['A tribute page', 'A survey', 'A newsletter', 'An event page', 'A prize draw', 'A music page', 'A photograph galery', 'An advertisement', 'A quiz', 'A CV'];
-var pageTopics = ['The Matrix', 'Harry Potter', 'Kung Fu Movies', 'Nigerian Food', 'Emma Watson', 'Will Smith', 'Jamaican Music', 'Elvis Presley', 'Bill Nye the Science Guy', 'Steve Jobs', ''];
-var htmlTags = ['img', 'h1 - h6', 'form', 'input', 'button', 'comments', 'hr', 'strong', 'blockquote', 'u', 'a', 'sup'];
-var cssProperties =['border-radius', 'box-shadow', 'text=sjadpw', 'background-color', '::hover', 'position', 'transform', 'line-height', 'font-family', 'overflow'];
+var pageTypes = ['A Tribute Page', 'A Survey', 'A Newsletter', 'An Event Page', 'A Prize Draw', 'A Music Page', 'A Photo Galery', 'An Advertisement', 'A Quiz'];
+var pageTopics = ['The Matrix', 'Harry Potter', 'Kung Fu Movies', 'Nigerian Food', 'Emma Watson', 'Will Smith', 'Jamaican Music', 'Elvis Presley', 'Bill Nye the Science Guy', 'Steve Jobs'];
+var htmlTags = ['<img>', '<h1> - <h6>', '<form>', '<input>', '<button>', 'comments <!-- -->', '<hr>', '<strong>', '<blockquote>', '<u>', '<a>', '<sup>'];
+var cssProperties =['border-radius', 'box-shadow', 'text-align', 'background-color', '::hover', 'position', 'transform', 'line-height', 'font-family', 'overflow'];
 
 console.log('ideaGenerator.js Loaded');
 
@@ -29,6 +29,26 @@ var cssProp3 = document.getElementById('cssProp3');
 var cssProp4 = document.getElementById('cssProp4');
 
 
+//find design brief elements
+
+var designBrief = document.getElementById('designBriefText');
+
+var pageTypeBrief = document.getElementById('pageTypeBrief');
+var pageThemeBrief = document.getElementById('pageThemeBrief');
+
+// html tags brief
+var htmlTagBrief1 = document.getElementById('htmlTagBrief1');
+var htmlTagBrief2 = document.getElementById('htmlTagBrief2');
+var htmlTagBrief3 = document.getElementById('htmlTagBrief3');
+var htmlTagBrief4 = document.getElementById('htmlTagBrief4');
+
+// CSS propertiess brief
+var cssPropBrief1 = document.getElementById('cssPropBrief1');
+var cssPropBrief2 = document.getElementById('cssPropBrief2');
+var cssPropBrief3 = document.getElementById('cssPropBrief3');
+var cssPropBrief4 = document.getElementById('cssPropBrief4');
+
+
 // functions
 
 // get random list integer
@@ -43,15 +63,87 @@ function clickGenerateButton() {
     var randomTypeIndex = getRandomInt(pageTypes.length);
     //insert suggested pageType
     pageTypeSuggestion.innerText = pageTypes[randomTypeIndex];
+    
+    // choose random page 
     var randomTopicIndex = getRandomInt(pageTopics.length);
+    //insert suggested pageTopic
+    topicSuggestion.innerText = pageTopics[randomTopicIndex];
 
+    //choose random html tags
+    var randomHtmlIndex1 = getRandomInt(htmlTags.length);
+    var randomHtmlIndex2 = getRandomInt(htmlTags.length);
+    var randomHtmlIndex3 = getRandomInt(htmlTags.length);
+    var randomHtmlIndex4 = getRandomInt(htmlTags.length);
+
+    // check that all prompts are different
+    while(randomHtmlIndex2 === randomHtmlIndex1){
+        randomHtmlIndex2 = getRandomInt(htmlTags.length);
+    };
+
+    while(randomHtmlIndex3 === randomHtmlIndex1 || randomHtmlIndex3 === randomHtmlIndex2){
+        randomHtmlIndex3 = getRandomInt(htmlTags.length);
+    };
+
+    while(randomHtmlIndex4 === randomHtmlIndex1 || randomHtmlIndex4 === randomHtmlIndex2 || randomHtmlIndex4 === randomHtmlIndex3){
+        randomHtmlIndex4 = getRandomInt(htmlTags.length);
+    };
+
+    // insert html tag suggestions
+    htmlTag1.innerText = htmlTags[randomHtmlIndex1];
+    htmlTag2.innerText = htmlTags[randomHtmlIndex2];
+    htmlTag3.innerText = htmlTags[randomHtmlIndex3];
+    htmlTag4.innerText = htmlTags[randomHtmlIndex4];
+
+
+
+    //choose random CSS props
+    var randomCSSIndex1 = getRandomInt(cssProperties.length);
+    var randomCSSIndex2 = getRandomInt(cssProperties.length);
+    var randomCSSIndex3 = getRandomInt(cssProperties.length);
+    var randomCSSIndex4 = getRandomInt(cssProperties.length);
+
+    // check that all prompts are different
+    while(randomCSSIndex2 === randomCSSIndex1){
+        randomCSSIndex2 = getRandomInt(cssProperties.length);
+    };
+
+    while(randomCSSIndex3 === randomCSSIndex1 || randomCSSIndex3 === randomCSSIndex2){
+        randomCSSIndex3 = getRandomInt(cssProperties.length);
+    };
+
+    while(randomCSSIndex4 === randomCSSIndex1 || randomCSSIndex4 === randomCSSIndex2 || randomCSSIndex4 === randomCSSIndex3){
+        randomCSSIndex4 = getRandomInt(cssProperties.length);
+    };
+
+    // insert CSS property suggestions
+    cssProp1.innerText = cssProperties[randomCSSIndex1];
+    cssProp2.innerText = cssProperties[randomCSSIndex2];
+    cssProp3.innerText = cssProperties[randomCSSIndex3];
+    cssProp4.innerText = cssProperties[randomCSSIndex4];
+
+
+
+    //change design brief
+    pageTypeBrief.innerText = pageTypes[randomTypeIndex].toLowerCase();
+    pageThemeBrief.innerText = pageTopics[randomTopicIndex];
+
+    htmlTagBrief1.innerText = htmlTags[randomHtmlIndex1];
+    htmlTagBrief2.innerText = htmlTags[randomHtmlIndex2];
+    htmlTagBrief3.innerText = htmlTags[randomHtmlIndex3];
+    htmlTagBrief4.innerText = htmlTags[randomHtmlIndex4];
+
+    cssPropBrief1.innerText = cssProperties[randomCSSIndex1];
+    cssPropBrief2.innerText = cssProperties[randomCSSIndex2];
+    cssPropBrief3.innerText = cssProperties[randomCSSIndex3];
+    cssPropBrief4.innerText = cssProperties[randomCSSIndex4];
 };
 
 
 newThemeButton.addEventListener('click', clickGenerateButton);
 
 
-//HTML with element ID's for reference
+//comment out the HTML with element ID's for reference / reduce typos by copy and pasting element ID
+
 {/* <div class="gridRow">
     <div class="gridSection">Page Type: </div> 
     <div id="pageTypeSuggestion" class="gridSection">Fan Page</div>
@@ -71,4 +163,15 @@ newThemeButton.addEventListener('click', clickGenerateButton);
 <div class="gridRow">
     <div class="gridSection">Using the CSS properties: </div> 
     <div id="cssPropSuggestion" class="gridSection"><span id="cssProp1">background-color</span>, <span id="cssProp2">width</span>, <span id="cssProp3">text-align</span>, <span id="cssProp4">margin</span></div>
-</div> */}
+</div> 
+
+<div class="sectionContent designBrief">
+                <p id="designBrief">
+                    This week, to practice my HTML and CSS skills, 
+                    I have made <span id="pageTypeBrief">(insert page type)</span> 
+                    about <span id="pageThemeBrief">(insert page theme)</span>.
+                    To make this project, I challenged myself to use the <span id="htmlTagBrief1">h1</span>, <span id="htmlTagBrief2">h2</span>, <span id="htmlTagBrief3">img</span>, <span id="htmlTagBrief4">form</span> tags in HTML.
+                    I also practiced using the <span id="cssPropBrief1">background-color</span>, <span id="cssPropBrief2">width</span>, <span id="cssPropBrief3">text-align</span>, <span id="cssPropBrief4">margin</span> to style the page</p>
+            </div>
+
+*/}
